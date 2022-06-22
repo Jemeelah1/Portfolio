2c3acf65f11820415fa338bf2b1a2f8620331e94
KolaJosh <ayoolak@ymail.com>
2022-06-22 12:48:14 +0100

    update: new cv and about text

--- src/components/About.js
+++ src/components/About.js
@@ -19,10 +19,10 @@ function About() {
             effective Frontend web developer and User Experience designer I have
             experience building beautiful user experiences and coordinating the
             development process to ensure that people are satisfied. I've also
-            taken on leadership roles to hone my leadership skills. I have
-            design experience with Adobe tools such as Photoshop, Illustrator,
-            and XD. I also design with Figma and am certain that I can get
-            proficient with any design tool. I also have web programming
+            taken on leadership roles to hone my leadership skills. <br />
+            <br />I have design experience with Adobe tools such as Photoshop,
+            Illustrator, and XD. I also design with Figma and am certain that I
+            can get proficient with any design tool. I also have web programming
             experience, having written code in javascript, reactjs, css, html,
             wordpress, node, mongodb, and other languages.
           </p>
